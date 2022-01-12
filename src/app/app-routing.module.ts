8e2d4ca350dad5cd21bd4df68ec71386f18eb06c
GeorgeHenriqueGH <georgehenriquecampos@gmail.com>
2022-01-12 17:43:14 -0300

    tecnico-create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
@@ -22,6 +23,10 @@ const routes: Routes = [
       {
         path: 'tecnicos',
         component: TecnicoListComponent
+      },
+      {
+        path: 'tecnicos/create',
+        component: TecnicoCreateComponent
       }
     ]
   }, 
